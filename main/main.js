e50d9df5e043141ad33bb8e3c67243485a29402e
kuirons <wugyu@outlook.com>
2017-05-26 20:16:25 +0800

    finished

--- main/main.js
+++ main/main.js
@@ -1,9 +1,25 @@
 // Write your cade below:
-function caclRemaindar() {};
+function caclRemaindar(a,b) {
+    return a%b
+};
 
-function caclSum() {};
+function caclSum(testArray) {
+    var sum=0;
+   for(var i=0;i<testArray.length;i++){
+       sum+=testArray[i]
+   }
+   return sum
+};
 
-function caclSumInConditon() {};
+function caclSumInConditon(testArray,variable) {
+    var sum=0
+    for(var i=0;i<testArray.length;i++){
+        if(testArray[i]<variable){
+            sum+=testArray[i]
+        }
+    }
+    return sum
+};
 
 module.exports = {
     caclRemaindar,
